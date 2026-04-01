f702eb9a5cae1a0e8e40ccd400ec6ec90084bdaa
Yasmin Ferrari Dias <yasminferraridias@gmail.com>
2026-04-01 16:00:26 -0300

    Formatação de data

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react'
 import { HistoryContainer, HistoryList, Status } from './styles'
 import { CyclesContext } from '../../contexts/CyclesContext'
-import { tr } from 'zod/v4/locales'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale/pt-BR'
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
@@ -25,13 +26,16 @@ export function History() {
               return (
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
-                  <td>{cycle.minutesAmount}</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>{formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR
+                  })}</td>
                   <td>
                     {cycle.finishedDate && (<Status statusColor="green">Concluído</Status>)}
-                  
+
                     {cycle.interruptedDate && (<Status statusColor="red">Interrompido</Status>)}
-                  
+
                     {!cycle.finishedDate && !cycle.interruptedDate && (
                       <Status statusColor="yellow">Em andamento</Status>
                     )}
